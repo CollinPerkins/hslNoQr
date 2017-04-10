c633aa948dea34de764edf640d2fe35057131989
Collin Perkins <collinperkinsweb@gmail.com>
2017-04-10 09:18:40 -0500

    changed location

--- src/components/app.js
+++ src/components/app.js
@@ -12,7 +12,7 @@ export default class App extends Component {
   this.state= {
     items: [],
     hasStarted: false,
-    location: "22-1-A",
+    location: "22-2-A",
     upc: " ",
     qr: [
     //   {
